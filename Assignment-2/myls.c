62bc98a826f9929c6e0f52c9b9da4a7fcf3651de
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2020-01-22 11:15:10 +0530

    System Commands

--- Assignment-2/myls.c
+++ Assignment-2/myls.c
@@ -8,21 +8,17 @@
 int main(int argc, char **argv)
 {
 
-	if (argc < 2)
-	{
-		fprintf(stderr, "Insufficient Arguments Supplied!\n");
-		return 1;
-	}
-
 	int dirfd;
 
 	DIR *dir;
 	struct dirent *ent;
 	struct stat fstat;
-	if (argc == 2)
+	if (argc < 3)
 	{
-
-		dir = opendir(argv[1]);
+		if(argc == 2)
+			dir = opendir(argv[1]);
+		else 
+			dir = opendir(".");
 
 		if (!dir)
 		{
