3d5f7153dfd9c2ed9cda4b23e9782b9515941b96
MaheshBharadwaj <MaheshBharadwaj101@gmail.com>
2020-04-05 22:30:21 +0530

    template

--- Assignment-08/LinkedList.h
+++ Assignment-08/LinkedList.h
@@ -11,8 +11,35 @@ typedef Node *List;
 List createEmptyList()
 {
     Node *head = (Node *)malloc(sizeof(Node));
-    initialise(&(head->d));
+    head->d.start = head->d.end = head->d.size = 0;
+    head->d.state = HOLE;
     head->next = NULL;
     return head;
 }
 
+void insert(List head, const Data d)
+{
+    Node *new = (Node *)malloc(sizeof(Node));
+    new->d = d;
+
+    Node *tmp = head;
+
+    while (tmp->next != NULL)
+    {
+        if (tmp->next->d.start > d.end)
+            break;
+        tmp = tmp->next;
+    }
+    new->next = tmp->next;
+    tmp->next = new;
+}
+
+void display(List head)
+{
+    Node *tmp = head->next;
+    while (tmp != NULL)
+    {
+        printf(" Start: %-3d  End: %-3d  State: %s\n", tmp->d.start, tmp->d.end, printState(tmp->d));
+        tmp = tmp->next;
+    }
+}
--- Assignment-08/MemoryAllocation.c
+++ Assignment-08/MemoryAllocation.c
@@ -10,9 +10,133 @@ typedef struct
     int state;
 } Partition;
 
-void initialise(Partition *const p)
+char *const printState(const Partition P)
 {
-    p->start = p->end = p->size = p->state = -1;
+    static char str[5];
+    if (P.state < -1)
+        exit(1);
+    else if (P.state == -1)
+        strcpy(str, "Hole");
+    else
+    {
+        str[0] = 'P';
+        str[1] = (P.state/10) + 48;
+        str[2] = (P.state%10) + 48;
+        str[3] = '\0';
+    }
+    return str;
 }
 
+#define HOLE -1
 #include "LinkedList.h"
+
+typedef enum Mode
+{
+    FirstFit,
+    BestFit,
+    WorstFit
+} Mode;
+
+int main()
+{
+
+    int choice = -1;
+    Mode m;
+
+    while (1)
+    {
+        printf("\t\t\tMEMORY ALLOCATION TECHNIQUES\n");
+        printf(" 1 - First Fit\n");
+        printf(" 2 - Best Fit\n");
+        printf(" 3 - Worst Fit\n");
+        printf(" 0 - Exit\n");
+        printf(" ------------------------\n");
+        printf(" Enter your choice: ");
+        scanf("%d", &m);
+
+        if (m < 0 || m > 3)
+        {
+            printf("Invalid Mode!\n");
+            continue;
+        }
+
+        printf("\n\n");
+        printf("\t\t\tOPTIONS\n");
+        printf(" 1 - Entry / Allocate\n");
+        printf(" 2 - Exit / De-Allocate\n");
+        printf(" 3 - Display\n");
+        printf(" 4 - Coalescing of Holes\n");
+        printf(" 5 - Back\n");
+        printf(" ------------------------\n");
+        printf(" Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice < 1 || choice > 5)
+        {
+            printf(" Invalid Input\n");
+            continue;
+        }
+
+        if (choice == 5)
+            continue;
+
+        switch (choice)
+        {
+        case 1:
+            switch (m)
+            {
+            case FirstFit:
+                //FFAlloc();
+                break;
+            case BestFit:
+                //BFAlloc();
+                break;
+            case WorstFit:
+                //WFAlloc();
+            default:
+                break;
+            }
+            break;
+        case 2:
+            switch (m)
+            {
+            case FirstFit:
+                //FFDealloc();
+                break;
+            case BestFit:
+                //BFDealloc();
+                break;
+            case WorstFit:
+                //WFDealloc();
+            default:
+                break;
+            }
+            break;
+        case 3:
+            //Display();
+            break;
+        case 4:
+            //Coalesce();
+        default:
+            break;
+        }
+    }
+
+    Partition tmp;
+    tmp.state = -1;
+    int n;
+    List head = createEmptyList();
+    printf(" Enter the number of blocks: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf(" Enter the start and end: ");
+        scanf("%d %d", &tmp.start, &tmp.end);
+        tmp.size = tmp.end - tmp.start;
+        insert(head, tmp);
+    }
+
+    printf(" Contents of the list:\n");
+    display(head);
+}
